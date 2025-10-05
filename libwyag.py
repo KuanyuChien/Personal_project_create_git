0681e9494e7a49118c89cbd40bb3e75a442a35cf
KuanyuChien <u1470293@utah.edu>
2025-10-05 14:14:28 -0500

    set up start main() function

--- libwyag.py
+++ libwyag.py
@@ -1,4 +1,35 @@
+'''
+Inspire by the website: https://wyag.thb.lt/?utm_source=chatgpt.com
+Create by Kuanyu Chien
+2025/10/05
+'''
+
 import argparse, configparser, grp, pwd, hashlib, os, re, sys, zlib
 from datetime import datetime
 from fnmatch import fnmatch
 from math import ceil
+
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
